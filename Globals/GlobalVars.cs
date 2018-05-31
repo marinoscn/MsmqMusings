93bbf7e4adbfa862e347e7f3f2192ae2e4b012e2
Marinos Christoforou <marinos@easymarkets.com>
2018-05-31 21:08:10 +0300

    Development Effort 1

--- Globals/GlobalVars.cs
+++ Globals/GlobalVars.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Globals
+namespace GlobalVars
 {
-    public static class Globals
+    public static class GlobalVars
     {
         // reference; https://stackoverflow.com/questions/11076790/the-bare-minimum-needed-to-write-a-msmq-sample-application
 
--- MsmqServer/Program.cs
+++ MsmqServer/Program.cs
@@ -3,26 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Messaging;
+using GlobalVars;
+
+
+//    MessageQueue messageQueue = null;
+//    if (MessageQueue.Exists(@".\Private$\SomeTestName"))
+//{
+//messageQueue = new MessageQueue(@".\Private$\SomeTestName");
+//messageQueue.Label = "Testing Queue";
+//}
+//else
+//{
+//// Create the Queue
+//MessageQueue.Create(@".\Private$\SomeTestName");
+//messageQueue = new MessageQueue(@".\Private$\SomeTestName");
+//messageQueue.Label = "Newly Created Queue";
+//}
+//messageQueue.Send("First ever Message is sent to MSMQ", "Title");
+
 
 namespace MsmqServer
 {
     class Program
     {
+        static MessageQueue _messageQueue = null;
+
         static void Main(string[] args)
         {
+            
             var exit = false;
             while (!exit)
             {
                 var message = Console.ReadLine();
-                if (message.Trim().ToLower() == "q")
-                    exit = true;
-                else
+                if (!string.IsNullOrWhiteSpace(message))
                 {
-                    // put message on message q
+                    if (message.Trim().ToLower() == "q")
+                        exit = true;
+                    else
+                    {
+                        // put message on message q
+                        if (!MessageQueue.Exists(GlobalVars.GlobalVars.MessageQueue))
+                        {
+                            CreateMessageQueue();
+                        }
+
+                        SendMessage(message);
+                    }
                 }
             }
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+        }
+
+        private static void SendMessage(string message)
+        {
+            _messageQueue = new MessageQueue(GlobalVars.GlobalVars.MessageQueue);
+            _messageQueue.Label = "Testing Queue";
+            _messageQueue.Send();
+        }
+
+        private static void CreateMessageQueue()
+        {
+            // Create the Queue
+            MessageQueue.Create(@".\Private$\SomeTestName");
+
         }
     }
 }
