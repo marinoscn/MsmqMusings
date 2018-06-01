1ecc29e4632b890835b9171bb6fa267915f09737
Marinos Christoforou <marinos@easymarkets.com>
2018-06-01 08:56:40 +0300

    Development Effort 2

---
+++ MsmqClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Messaging;
+
+namespace MsmqClient
+{
+    class Program
+    {
+        static MessageQueue _messageQueue;
+
+        static void Main(string[] args)
+        {
+            _messageQueue = new MessageQueue(GlobalVars.GlobalVars.MessageQueue);
+            _messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+            var messages = _messageQueue.GetAllMessages();
+
+            foreach (System.Messaging.Message message in messages)
+            {
+                try
+                {
+                    var text = (string)message.Body;
+                    Console.WriteLine(text);
+                }
+                catch (MessageQueueException ex)
+                {
+                    Console.WriteLine($"Message Queue Exception: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"General Exception: {ex.Message}");
+                }
+            }
+            _messageQueue.Purge();
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+        }
+    }
+}
--- MsmqServer/Program.cs
+++ MsmqServer/Program.cs
@@ -1,40 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Messaging;
-using GlobalVars;
-
-
-//    MessageQueue messageQueue = null;
-//    if (MessageQueue.Exists(@".\Private$\SomeTestName"))
-//{
-//messageQueue = new MessageQueue(@".\Private$\SomeTestName");
-//messageQueue.Label = "Testing Queue";
-//}
-//else
-//{
-//// Create the Queue
-//MessageQueue.Create(@".\Private$\SomeTestName");
-//messageQueue = new MessageQueue(@".\Private$\SomeTestName");
-//messageQueue.Label = "Newly Created Queue";
-//}
-//messageQueue.Send("First ever Message is sent to MSMQ", "Title");
-
 
 namespace MsmqServer
 {
     class Program
     {
-        static MessageQueue _messageQueue = null;
+        static MessageQueue _messageQueue;
+        private static int _counter;
 
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            Console.WriteLine($"Enter some messages, type 'q' to quit{Environment.NewLine}");
             var exit = false;
             while (!exit)
             {
+
                 var message = Console.ReadLine();
                 if (!string.IsNullOrWhiteSpace(message))
                 {
@@ -52,21 +32,19 @@ namespace MsmqServer
                     }
                 }
             }
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
         }
 
         private static void SendMessage(string message)
         {
             _messageQueue = new MessageQueue(GlobalVars.GlobalVars.MessageQueue);
-            _messageQueue.Label = "Testing Queue";
-            _messageQueue.Send();
+            _messageQueue.Label = "Server Queue";
+            _messageQueue.Send(message, "Message #: " + (++_counter).ToString() );
         }
 
         private static void CreateMessageQueue()
         {
             // Create the Queue
-            MessageQueue.Create(@".\Private$\SomeTestName");
+            MessageQueue.Create(GlobalVars.GlobalVars.MessageQueue);
 
         }
     }
